d991adbcd767500d4a5f543d2ce0e68ac67831da
Aurélien Riche <contact@aurelienriche.com>
2020-09-09 14:09:27 +0200

    Json & Services base

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import * as data from '../data/data.json';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor() { }
+
+  public get posts(): any {
+    return data;
+  }
+}
