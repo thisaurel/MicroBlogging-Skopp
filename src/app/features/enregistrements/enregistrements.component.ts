e06a0035851e9b53ca7ff37158882d789da3cd3c
Aurélien Riche <contact@aurelienriche.com>
2020-09-09 16:49:24 +0200

    Fix

--- src/app/features/enregistrements/enregistrements.component.ts
+++ src/app/features/enregistrements/enregistrements.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-enregistrements',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnregistrementsComponent implements OnInit {
 
-  constructor() { }
+  public postsList = [];
+  
+  constructor(
+    public postService: PostService
+  ) { }
 
   ngOnInit(): void {
+    this.postsList = this.postService.forHomePage;
   }
 
 }
